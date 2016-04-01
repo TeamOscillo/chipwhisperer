a6946a964b047d330f864bf217a666f7d5d4d4c1
trustkim <trustkim@users.noreply.github.com>
2016-04-01 17:36:14 +0900

    Update lea_keyschedule.h

--- hardware/victims/firmware/crypto/avrcryptolib/lea/lea_keyschedule.h
+++ hardware/victims/firmware/crypto/avrcryptolib/lea/lea_keyschedule.h
@@ -1,7 +1,7 @@
 /* lea_keyschedule.h */
 /*
     This file is part of the AVR-Crypto-Lib.
-    Copyright (C) 2016  TeamOscillo (qhdwls202@naver.com)
+    Copyright (C) 2016  TeamOscillo <qhdwls202@naver.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
