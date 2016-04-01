76bef170d9a47ccda39ae95c286332ce3f7d6fd1
trustkim <trustkim@users.noreply.github.com>
2016-04-01 16:58:40 +0900

    modify comments

--- hardware/victims/firmware/crypto/avrcryptolib/lea/lea_enc.h
+++ hardware/victims/firmware/crypto/avrcryptolib/lea/lea_enc.h
@@ -1,20 +1,28 @@
 /* lea_enc.h */
 /*
- This file is part of the AVR-Crypto-Lib.
- Copyright TeamOscillo (qhdwls202@naver).
- 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+    This file is part of the AVR-Crypto-Lib.
+    Copyright 2016  TeamOscillo (qhdwls202@naver)
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 /**
  * \file     lea_enc.h
  * \email    qhdwls202@naver.com
  * \author   TeamOscillo
  * \date     2016-03-14
- **/
+ * \license  GPLv3 or later
+ */
 #ifndef LEA_ENC_H_
 #define LEA_ENC_H_
 #include "lea_types.h"
@@ -25,4 +33,3 @@ void lea_encrypt_core(lea_cipher_state_t* state,const LEA_KEY *key);
 
 
 #endif
-
