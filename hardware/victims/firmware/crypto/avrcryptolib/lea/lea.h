b6076b177718210688524c21d4b40a9bf93427ba
trustkim <trustkim@users.noreply.github.com>
2016-04-01 16:29:10 +0900

    modify comments
    we want to keep track GPLv3 license

--- hardware/victims/firmware/crypto/avrcryptolib/lea/lea.h
+++ hardware/victims/firmware/crypto/avrcryptolib/lea/lea.h
@@ -1,20 +1,28 @@
 /* lea.h */
 /*
- This file is part of the AVR-Crypto-Lib.
- Copyright TeamOscillo (qhdwls202@naver).
+    This file is part of the AVR-Crypto-Lib.
+    Copyright TeamOscillo (qhdwls202@naver).
  
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 /**
  * \file     lea.h
  * \email    qhdwls202@naver.com
  * \author   TeamOscillo
  * \date     2016-03-14
- **/
+ * \license  GPLv3 or later
+ */
 #ifndef LEA_H_
 #define LEA_H_
 
@@ -37,4 +45,3 @@
 #include "lea_keyschedule.h"
 
 #endif
-
