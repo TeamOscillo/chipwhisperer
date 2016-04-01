4934999aea1c219563b756fe7fca3c7fe6304f44
trustkim <trustkim@users.noreply.github.com>
2016-04-01 22:55:05 +0900

    Update lea-independant.h

--- hardware/victims/firmware/crypto/lea-independant.h
+++ hardware/victims/firmware/crypto/lea-independant.h
@@ -1,18 +1,27 @@
 /*
- This file is part of the  LEAExplorer Example Targets
- Copyright TeamOscillo (qhdwls202@naver).
- 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+    This file is part of the ChipWhisperer Example Targets
+    Copyright (C) 2016 TeamOscillo (qhdwls202@naver)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 /**
  * \file     lea-independant.h
  * \email    qhdwls202@naver.com
  * \author   TeamOscillo
  * \date     2016-03-14
- **/
+ * \license  GPLv3 or later
+ */
 #ifndef LEA_INDEPENDANT_
 #define LEA_INDEPENDANT_
 
@@ -44,4 +53,3 @@ void lea_indep_key(uint8_t * key);
 void lea_indep_enc(uint8_t * pt);
 
 #endif
-
